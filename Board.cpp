8ce08a0df43df12651055d33e2a166a82d91157a
ShOFu2 <alelsandur608@gmail.com>
2024-12-02 12:36:40 +0500

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+void printBoard(const vector<int>& board) {
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            if (board[i * 4 + j] == 0)
+                cout << "   ";
+            else
+                cout << board[i * 4 + j] << " ";
+        }
+        cout << endl;
+    }
+}
