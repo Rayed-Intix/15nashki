95c357516ccdbf67a8e95d47729a4a1946f459fd
ShOFu2 <alelsandur608@gmail.com>
2024-12-02 12:51:30 +0500

    Update Board.cpp
    board initialization

--- Board.cpp
+++ Board.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void printBoard(const vector<int>& board) {
+void printBoard(const vector<int> board) {
     for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 4; ++j) {
             if (board[i * 4 + j] == 0)
                 cout << "   ";
             else
-                cout << board[i * 4 + j] << " ";
+                cout << board[i * 4 + j] << "\t";
         }
         cout << endl;
     }
+}
+
+void initBoard(vector<int>& board){
+    for (int i = 0; i < 15; i++) {
+        board[i] = i + 1;
+    }
+    board[15] = 0;
+    printBoard(board);
 }
