d2d31910db8fd5b0a38d3e9f3957825e81505e2a
ShOFu2 <alelsandur608@gmail.com>
2024-12-09 12:08:03 +0500

    Update header.h

--- header.h
+++ header.h
@@ -6,4 +6,4 @@ void initBoard(int[4][4]);
 void shuffleBoard(int[4][4]);
 void findCoord(int[4][4], int&, int&);
 bool Check(int[4][4]);
-void Movement(int[4][4], int&, int&);
+void Movement(int[4][4], int&, int&, char);
